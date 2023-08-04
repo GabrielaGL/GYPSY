59bbf5e3d63592252d8e426d3e09ff9c82d480b8
Gaabs <ggaabrielagomez@gmail.com>
2023-08-03 19:52:05 -0600

    SVG de guft card cambia segun selector

--- src/index.js
+++ src/index.js
@@ -5,28 +5,23 @@ import { isValid, maskify } from "./validator.js";
 
 document.querySelector("#pago").addEventListener("click", validateNumber);
 
+/* Selector changes background og gift card */
+const optionCard = document.getElementById('bgCard');
+optionCard.addEventListener('change', (event) => {
+  document.getElementById('bgGiftCard').src = "./imgs/" + event.target.value + ".svg"
+})
+
 
 function validateNumber() {
   const cardNumber = document.getElementById("cardNumber").value;
   // Condicional que alerta si es o no es un numero    
-  if (isNaN(cardNumber) || cardNumber.length < 1 ) {
+  if (isNaN(cardNumber) || cardNumber.length < 1) {
     document.getElementById("result").innerHTML = "Ingresa un numero de tarjeta válido";
-    console.log("Entro a la funcion");
   } else {
-      if (isValid(cardNumber) === true) {
-      console.log("entro al final")
+    if (isValid(cardNumber) === true) {
       document.getElementById("result").innerHTML = "La tarjeta " + maskify(cardNumber) + " es válida";
     } else {
       document.getElementById("result").innerHTML = "La tarjeta no es válida. Intente de nuevo";
     }
   }
 }
-
-
-// Probablemente colapsa por tantos if; simplifica en codigo
-
-
-
-// maskify(parsed)
-// console.log("Los numeros son " + maskify)
-// isValid();
